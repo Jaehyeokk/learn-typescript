eaadd08a3efa97ec1b7e9e8128cb40f7e7c81deb
Jaehyeokk <lionjk92@naver.com>
2022-01-16 03:05:24 +0900

    할 일 관리 앱 - 추가 기능 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
     <h1>Vue Todo with Typescript</h1>
+    <TodoInput
+      :item="todoText"
+      @input="updateTodoText"
+      @add="addTodo"
+    ></TodoInput>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import TodoInput from "./components/TodoInput.vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: "",
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodo() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = "";
+    },
+  },
+});
 </script>
 
 <style scoped></style>
